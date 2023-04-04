ad73252c6e5e5097ffb6a8489c65679c24f7c37f
Abhay Ruparel <abhayruparel2000@gmail.com>
2023-04-04 23:14:07 +0530

    user: fix collect address

--- user/collectAddress.php
+++ user/collectAddress.php
@@ -1,47 +1,16 @@
 <?php
 require('../config.php');
 include('userHeader.php');
-
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Escape user inputs for security
-    $name = mysqli_real_escape_string($con, $_POST['name']);
-    $email = mysqli_real_escape_string($con, $_POST['email']);
-    $phone = mysqli_real_escape_string($con, $_POST['phone']);
-    $addrs = mysqli_real_escape_string($con, $_POST['addrs']);
-    $pincode = mysqli_real_escape_string($con, $_POST['pincode']);
-    $state = mysqli_real_escape_string($con, $_POST['state']);
-    $country = mysqli_real_escape_string($con, $_POST['country']);
-    $user_id_fk = $_SESSION['user_id'];
-
-    // Validate pincode
-    if (!preg_match('/^\d{6}$/', $pincode)) {
-        echo "Invalid pincode format";
-        exit();
-    }
-
-    // Insert address into database
-    $sql = "INSERT INTO address (name, email, phone, addrs, pincode, state, country, user_id_fk)
-        VALUES ('$name', '$email', '$phone', '$addrs', '$pincode', '$state', '$country', '$user_id_fk')";
-
-    if (mysqli_query($con, $sql)) {
-        echo "Address added successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($con);
-    }
-
-
-}
 ?>
 
 <br>
 <div id="page-wrapper">
-<form action="" method="POST">
+<form action="collectAddress_post.php" method="POST">
   <label for="name">Address Name:</label>
   <input type="text" id="name" name="name" required><br>
 
   <label for="phone">Phone:</label>
-  <input type="text" id="phone" name="phone"><br>
+  <input type="text" id="phone" name="phone" required><br>
 
   <label for="addrs">Address:</label>
   <input type="text" id="addrs" name="addrs" required><br>
