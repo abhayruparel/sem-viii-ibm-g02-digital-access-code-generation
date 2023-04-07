0751efa319e1a0ba2184ff7b2f9105e97b2d214d
Abhay Ruparel <abhayruparel2000@gmail.com>
2023-04-07 22:19:28 +0530

    user: add address: add validation

--- user/collectAddress.php
+++ user/collectAddress.php
@@ -2,10 +2,15 @@
 require('../config.php');
 include('userHeader.php');
 ?>
-
+<style>
+  input{
+    width: 50%;
+  }
+</style>
 <br>
 <div id="page-wrapper">
-<form action="collectAddress_post.php" method="POST">
+
+<form action="collectAddress_post.php" method="POST" onsubmit="return validateForm()">
   <label for="name">Address Name:</label>
   <input type="text" id="name" name="name" required><br>
 
@@ -27,7 +32,37 @@ include('userHeader.php');
 
   <input type="submit" value="Submit">
 </form></div>
-
+<script>
+function validateForm() {
+  var name = document.getElementById("name").value;
+  var phone = document.getElementById("phone").value;
+  var addrs = document.getElementById("addrs").value;
+  var pincode = document.getElementById("pincode").value;
+  var state = document.getElementById("state").value;
+  var country = document.getElementById("country").value;
+  
+  var nameRegex = /^[a-zA-Z]+$/;
+  var phoneRegex = /^[0-9]{10}$/;
+  var pincodeRegex = /^[0-9]{6}$/;
+  
+  if (!nameRegex.test(name)) {
+    alert("Please enter a valid name (only letters allowed)");
+    return false;
+  }
+  
+  if (!phoneRegex.test(phone)) {
+    alert("Please enter a valid phone number (10 digits only)");
+    return false;
+  }
+  
+  if (!pincodeRegex.test(pincode)) {
+    alert("Please enter a valid pincode (6 digits only)");
+    return false;
+  }
+  
+  return true;
+}
+</script>
 <?php
 include('userFooter.php');
 ?>
